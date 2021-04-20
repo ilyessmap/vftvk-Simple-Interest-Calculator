f4c06df3f5d3094db7fb57ce2c55fdea59e73ae6
ilyessmap <56192505+ilyessmap@users.noreply.github.com>
2021-04-20 17:28:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) { // zero or negative
+        alert("Enter a positive number");
+        document.getElementById("principal").focus(); // take user back to "Principal" input box
+    } else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+        // Puts everything together and pushes to the "result" element
+        document.getElementById("result").innerHTML = `If you deposit <span class="highlight">${principal}</span>,<br />at an interest rate of <span class="highlight">${rate}%</span>.<br />You will receive an amount of <span class="highlight">${interest}</span>,<br />in the year <span class="highlight">${year}</span>`;
+    }
 }
-        
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+function clearResult() {
+    document.getElementById("result").innerHTML = "";
+}
